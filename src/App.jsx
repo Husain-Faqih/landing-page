274a79e37d5514e8d10e9da49f25e0f3a05e3cb2
husen <obatherbal455@gmail.com>
2026-09-09 13:01:17 +0700

    tambah useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
+import { useEffect } from "react";
+
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Features from "./components/Feature";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import { useEffect } from "react";
 
 function App() {
   useEffect(() => {
     const handleScroll = () => {
-      const navbar = document.querySelector("./navbar");
+      const navbar = document.querySelector(".navbar");
 
       if (window.scrollY > 50) {
         navbar.classList.add("scrolled");
