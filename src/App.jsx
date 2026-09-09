56770e8621dfa0dd4f170122f3ceb202610276b3
husen <obatherbal455@gmail.com>
2026-09-09 12:46:21 +0700

    tambah useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,27 @@ import About from "./components/About";
 import Features from "./components/Feature";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const handleScroll = () => {
+      const navbar = document.querySelector("./navbar");
+
+      if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+      } else {
+        navbar.classList.remove("scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
       <Navbar />
